0fe51a294a9a7ae0aa6c303a37a0cc86900a70d3
Michael Boyer <mboy221@aucklanduni.ac.nz>
2026-07-16 12:35:05 +1200

    Reworded metadata export to statistics

--- app/(app)/statistics/page.tsx
+++ app/(app)/statistics/page.tsx
@@ -3,7 +3,7 @@ import Statistics from "./StatisticsPage";
 export async function generateMetadata() {
     const url = process.env.NEXT_PUBLIC_APP_URL;
     return {
-        title: "Key Statistics",
+        title: "Statistics",
         description: "View how your applications have been performing.",
         alternates: {
             canonical: url + "/statistics"
